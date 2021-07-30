d3ad83bfba84b5c218dacf115bac56b9beb658d7
Tamim <tamim-sediqi@gmx.de>
2021-07-30 10:08:57 +0200

    Erste Änderung

--- MeinProjekt/src/KeineAhnung/HalloWlet.java
+++ MeinProjekt/src/KeineAhnung/HalloWlet.java
@@ -1,5 +1,8 @@
 package KeineAhnung;
 
 public class HalloWlet {
-
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt!");
+        System.out.println("Zweite Zeile!");
+    }
 }
