774760692eb896f414838df2ec7dd10d55cc8d10
Tamim <tamim-sediqi@gmx.de>
2021-08-02 15:59:09 +0200

    whileSchleifeübung

---
+++ MeinProjekt/src/Programme/whilSchleife.java
@@ -0,0 +1,15 @@
+package Programme;
+
+public class whilSchleife {
+    public static void main(String[] args) {
+        int nummer = 1;
+        while(nummer <= 1000000000) {
+            System.out.println(nummer);
+            nummer = nummer +1;
+
+        }
+
+    }
+
+
+}
