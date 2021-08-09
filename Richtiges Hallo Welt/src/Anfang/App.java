f79ec9e4b263e0e74b6ce2a8fcd228b8d326bfca
TamimIlyas <75974165+TamimIlyas@users.noreply.github.com>
2021-08-09 13:27:57 +0200

    Neu Projekt

---
+++ Richtiges Hallo Welt/src/Anfang/App.java
@@ -0,0 +1,22 @@
+package Anfang;
+
+public class App {
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+
+        public static int doubleTheNumber(int number) {
+            return 2;
+        }
+
+        private static int add(String Eingabe) {
+            System.out.println(Eingabe);
+            int sum = 0;
+            //for (int i = 0; i > sum; i++ )
+            return -1;
+            //return sum;
+        }
+    }
+
+
