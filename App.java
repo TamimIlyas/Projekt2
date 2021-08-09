cd6cfd9ed97bcd34c90c524ef1ad784e6283bb78
TamimIlyas <75974165+TamimIlyas@users.noreply.github.com>
2021-08-09 13:47:56 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int doubleTheNumber(int number) {
+        return 2;
+    }
+
+    private static int add(String Eingabe) {
+    System.out.println(Eingabe);
+    int sum = 0;
+    //for (int i = 0; i > sum; i++ )
+        return -1;
+        //return sum;
+    }
+}
